f54ab0ce92cc49c056a241ead351bd86c72f355a
binhquocnguyen <binhquoc7504@gmail.com>
2023-10-13 17:00:36 +0700

    fix: Code Ex_04

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,15 @@
 const removeFromArray = function() {
+    for (let i = 0; i <= (arguments[0].length - 1); i++) {
+        for (let j = 1; j <= (arguments.length -1); j++) {
+            if (arguments[0][i] === arguments[j]) {
+                arguments[0].splice(i, 1);
+                i--;
+                break;
+            }
+        }
+    }
 
+    return arguments[0];
 };
 
 // Do not edit below this line
