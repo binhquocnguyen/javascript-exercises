b600f023a749aeb3b5d12f99380471748ecd5fd8
binhquocnguyen <binhquoc7504@gmail.com>
2023-10-13 20:40:40 +0700

    fix: Code Ex_07

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,11 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(inputF) {
+  let outputC = (inputF - 32) * 5/9;
+  return Math.round(outputC * 10) / 10; 
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(inputC) {
+  let outputF = (inputC * 9/5 + 32);
+  return Math.round(outputF * 10) / 10;
 };
 
 // Do not edit below this line
