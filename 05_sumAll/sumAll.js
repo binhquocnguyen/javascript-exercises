df990e2be7f1d48027c1eaf41c2b889645336bf9
binhquocnguyen <binhquoc7504@gmail.com>
2023-10-13 19:16:39 +0700

    fix: Code Ex_05

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,18 @@
-const sumAll = function() {
+const sumAll = function(firstNum, secondNum) {
+    if ( !(typeof firstNum === 'number') || !(typeof secondNum === 'number') ) {
+        return 'ERROR';
+    } else if (firstNum < 0 || secondNum < 0) {
+        return 'ERROR';
+    } else if (firstNum > secondNum) { //Case: firstNum > secondNum
+        [firstNum, secondNum] = [secondNum, firstNum];
+    } 
 
+    let sum = 0;
+    for (i = firstNum; i <= secondNum; i++) {
+        sum += i;
+    }
+
+    return sum;
 };
 
 // Do not edit below this line
