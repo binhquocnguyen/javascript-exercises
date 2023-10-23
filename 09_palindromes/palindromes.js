40ed49ba63507ddbd87fc08a4ad2bc52ba125ae4
binhquocnguyen <binhquoc7504@gmail.com>
2023-10-24 00:05:53 +0700

    fix: Code Ex_09

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,14 @@
-const palindromes = function () {
-
+const palindromes = function (str) {
+    let forStr = str
+                .toLowerCase() 
+                .split('')
+                .filter( item => (item >= 'a' && item <= 'z') || (item >= '0' && item <= '9') && !(item === ' ') )
+                .join('');
+    let backStr = forStr
+                .split('')
+                .reverse()
+                .join('');
+    return (forStr == backStr) ? true : false;
 };
 
 // Do not edit below this line
