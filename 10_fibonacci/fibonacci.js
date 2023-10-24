14f0824fea021db1dcf62489dab970e4551b8a66
binhquocnguyen <binhquoc7504@gmail.com>
2023-10-24 11:28:04 +0700

    fix: Code Ex_10

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,16 @@
-const fibonacci = function() {
+const fibonacci = function(num) {
+    if (num < 0) return "OOPS";
+    if (num == 0) return 0;
 
+    let arr = [];
+    for (let i = 0; i < num; i++) {
+        if (i == 0 || i == 1) {
+            arr.push(1);
+        } else {
+            arr.push(arr[i-2] + arr[i-1]);
+        }
+    }
+    return arr[num-1];
 };
 
 // Do not edit below this line
